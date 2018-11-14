c6fb774efea0b8b2533a58f1e77aa1513426f83e
zhucy91 <zhucy91@163.com>
2018-11-14 23:45:42 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: zhucy
+  Date: 2018/11/14
+  Time: 23:06
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>朱长源</title>
+  </head>
+  <body>
+  <h1 style="color: red">zhucy</h1>
+  </body>
+</html>
---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,42 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * 快捷键的使用
+ *
+ * 1 执行(run)  alt+r
+ * 2 提示补全 (Class Name Completion)    alt+/
+ * 3 单行注释       ctrl + /
+ * 4 多行注释 ctrl + shift + /
+ * 5 向下复制一行 (Duplicate Lines)    ctrl+alt+down
+ * 6 删除一行或选中行 (delete line)    ctrl+d
+ * 7 向下移动行(move statement down)    alt+down
+ * 8 向上移动行(move statement up)     alt+up
+ * 9 向下开始新的一行(start new line)    shift+enter
+ * 10 向上开始新的一行 (Start New Line before current)      ctrl+shift+enter
+ * 11 如何查看源码 (class)     ctrl + 选中指定的结构 或 ctrl + shift + t
+ * 12 万能解错/生成返回值变量     alt + enter
+ * 13 退回到前一个编辑的页面 (back)    alt + left
+ * 14 进入到下一个编辑的页面(针对于上条) (forward)    alt + right
+ * 15 查看继承关系(type hierarchy)    F4
+ */
+public class Customer {
+
+    public static  void main(String[]args){
+
+
+        System.out.println("Hello World");
+
+
+        new ArrayList()
+        Date date = new Date();
+
+
+
+    }
+
+
+
+}
